1fa450a460646bf9eb23000964406e5ed6029424
thp <hong.ping.tan@gmail.com>
2018-07-27 10:08:21 +0800

    Initial maven project setup

---
+++ src/test/java/org/companyname/AppTest.java
@@ -0,0 +1,25 @@
+package org.companyname;
+
+import org.junit.Test;
+
+import junit.framework.*;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void testNegative()
+    {
+        assertTrue( false );
+    }
+}
